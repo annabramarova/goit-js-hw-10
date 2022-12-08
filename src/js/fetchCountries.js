4c070aedf9cb8582f314a0f5fe3c9257e028ac15
Anna Bramarova <annabramarova@gmail.com>
2022-12-08 18:13:35 +0200

    пробный async await

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,11 +1,9 @@
 const URL = "https://restcountries.com/v3.1/name/"
 
-export function fetchCountries(name) {
-    return fetch(`${URL}${name}?fields=name,capital,population,flags,languages`)
-        .then(r => {
-            if (!r.ok || r.status === 404) {
-                throw new Error(r.status);
+export async function fetchCountries(name) {
+    const result = await fetch(`${URL}${name}?fields=name,capital,population,flags,languages`);
+        if (!result.ok || result.status === 404) {
+                throw new Error(result.status);
             }
-            return r.json();
-        });
+    return result.json();
 }
